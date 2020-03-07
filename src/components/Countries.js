9e27a3ae009fef2defa3c5dd6a1a816336ea54b4
PioKl <kosiu342@gmail.com>
2020-03-07 21:56:22 +0100

    Add NavLink to every country

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import '../style/Countries.scss'
 const Countries = (props) => {
 
@@ -7,9 +8,12 @@ const Countries = (props) => {
     }
 
     const countries = props.countriesAll.map((country, index) => (
+
         <div className="country-container" key={index}>
             <img className="flagImg" src={country.flag} alt={`${country.name} flag`} />
-            <p>{country.name}</p>
+            {/* country.cioc ? country.cioc : country.name - jesli ma cioc to uzywaj w celu dobrego linkowanie z borderami, ktore posluguja sie skrotami, a cioc to skrot */}
+            {/* badz sposob z alpha3Code zeby bylo zgodne z linkami z border, np border IRN to w alpha3Code Iranu tez IRN, ale juz cioc to IRI wiec linki sa sprzeczne, kilka jest takich wyjatkow, alpha3Code===border */}
+            <NavLink to={country.alpha3Code} className="navLink"><p>{country.name}</p></NavLink>
             <ul>
                 <li>Population: {populationDotted(country.population)}</li>
                 <li>Region: {country.region}</li>
@@ -18,10 +22,11 @@ const Countries = (props) => {
         </div>
     ))
     return (
-        <div className='countries-container'>
-            {countries}
-        </div>
-
+        <>
+            <div className='countries-container'>
+                {countries}
+            </div>
+        </>
     );
 }
 
