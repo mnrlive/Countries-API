dd8d1b0c36da9153e2876c604c08785e2c8b2fd7
PioKl <kosiu342@gmail.com>
2020-03-09 14:49:46 +0100

    Add early version of input and handler(dark/light)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
-import React from 'react';
-const Header = () => {
-    return (
-        <div>
-            <p>MenuPlaceholder</p>
-        </div>
-    );
+import React, { Component } from 'react';
+class Header extends Component {
+    state = {
+        darkMode: false,
+    }
+
+    handleSlider = () => {
+        this.setState({
+            darkMode: !this.state.darkMode,
+        })
+    }
+    render() {
+        return (
+            <div>
+                <p>MenuPlaceHolder</p>
+                <label htmlFor="">
+                    <input type="checkbox" name="" id="" onChange={this.handleSlider} />
+                </label>
+            </div>
+        );
+    }
 }
 
 export default Header;
