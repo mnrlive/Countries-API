e221449473a926050f53a529a7c7d90e40560f2c
PioKl <kosiu342@gmail.com>
2020-03-09 14:47:26 +0100

    Change: NavLink applies now to the all div content

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -8,17 +8,18 @@ const Countries = (props) => {
     }
 
     const countries = props.countriesAll.map((country, index) => (
-
         <div className="country-container" key={index}>
-            <img className="flagImg" src={country.flag} alt={`${country.name} flag`} />
             {/* country.cioc ? country.cioc : country.name - jesli ma cioc to uzywaj w celu dobrego linkowanie z borderami, ktore posluguja sie skrotami, a cioc to skrot */}
             {/* badz sposob z alpha3Code zeby bylo zgodne z linkami z border, np border IRN to w alpha3Code Iranu tez IRN, ale juz cioc to IRI wiec linki sa sprzeczne, kilka jest takich wyjatkow, alpha3Code===border */}
-            <NavLink to={country.alpha3Code} className="navLink"><p>{country.name}</p></NavLink>
-            <ul>
-                <li>Population: {populationDotted(country.population)}</li>
-                <li>Region: {country.region}</li>
-                <li>Capital: {country.capital}</li>
-            </ul>
+            <NavLink to={country.alpha3Code} className="navLink">
+                <img className="flagImg" src={country.flag} alt={`${country.name} flag`} />
+                <p>{country.name}</p>
+                <ul>
+                    <li>Population: {populationDotted(country.population)}</li>
+                    <li>Region: {country.region}</li>
+                    <li>Capital: {country.capital}</li>
+                </ul>
+            </NavLink>
         </div>
     ))
     return (
