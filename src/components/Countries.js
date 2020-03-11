726135d30647c2e04cc88687bb6d5f720ddbcba0
PioKl <kosiu342@gmail.com>
2020-03-11 21:07:24 +0100

    Add new divs, classes, NavLink with div place swap

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -8,19 +8,25 @@ const Countries = (props) => {
     }
 
     const countries = props.countriesAll.map((country, index) => (
-        <div className="country-container" key={index}>
+
+        <NavLink to={country.alpha3Code} className="navLink" key={index}>
             {/* country.cioc ? country.cioc : country.name - jesli ma cioc to uzywaj w celu dobrego linkowanie z borderami, ktore posluguja sie skrotami, a cioc to skrot */}
             {/* badz sposob z alpha3Code zeby bylo zgodne z linkami z border, np border IRN to w alpha3Code Iranu tez IRN, ale juz cioc to IRI wiec linki sa sprzeczne, kilka jest takich wyjatkow, alpha3Code===border */}
-            <NavLink to={country.alpha3Code} className="navLink">
-                <img className="flagImg" src={country.flag} alt={`${country.name} flag`} />
-                <p>{country.name}</p>
-                <ul>
-                    <li>Population: {populationDotted(country.population)}</li>
-                    <li>Region: {country.region}</li>
-                    <li>Capital: {country.capital}</li>
-                </ul>
-            </NavLink>
-        </div>
+            <div className="countryCard" key={index}>
+                <div className="countryCard__flag">
+                    <img className="countryCard__flagImg" src={country.flag} alt={`${country.name} flag`} />
+                </div>
+                <div className="countryCard__description">
+                    <h1 className="countryCard__primary">{country.name}</h1>
+                    <ul className="countryCard__itemList">
+                        <li className="countryCard__item"><span>Population: </span>{populationDotted(country.population)}</li>
+                        <li className="countryCard__item"><span>Region: </span>{country.region}</li>
+                        <li className="countryCard__item"><span>Capital: </span>{country.capital}</li>
+                    </ul>
+                </div>
+
+            </div>
+        </NavLink>
     ))
     return (
         <>
