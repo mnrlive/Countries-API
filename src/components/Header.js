762b9a924e3611e9e7486147366d0358b427d325
PioKl <kosiu342@gmail.com>
2020-03-11 21:13:24 +0100

    Add header class

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import '../style/Header.scss';
+/* import Icons from '../img/sprite.svg'; */
+
 class Header extends Component {
     state = {
         darkMode: false,
@@ -11,14 +14,19 @@ class Header extends Component {
     }
     render() {
         return (
-            <div>
+            <header className="header">
                 <p>MenuPlaceHolder</p>
-                <label htmlFor="">
+                <label htmlFor="" className='darkLightmode'>
+                    {/* <svg className="darkLightmode__icon"> */}
+                    {/* <use xlinkHref={`${Icons}#icon-moon}`}></use> */}
+                    {/* <use xlinkHref="img/sprite.svg#icon-moon"></use> */}
+                    {/* </svg> */}
                     <input type="checkbox" name="" id="" onChange={this.handleSlider} />
                 </label>
-            </div>
+            </header>
         );
     }
 }
+
 
 export default Header;
