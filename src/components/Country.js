7046ea2e8a8df41975fa08e8a7ef8d2a93f399d8
PioKl <kosiu342@gmail.com>
2020-03-13 19:51:46 +0100

    New classes, divs

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,48 +1,80 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import "../style/Country.scss";
+
 const Country = (props) => {
 
     const topLevelDomain = props.topLevelDomain.map((domain, index) => (
-        <li key={index}>{domain}</li>
+        <li className="info-item" key={index}>{domain}</li>
     ))
     const languages = props.languages.map((language, index) => (
-        <li key={index}>{language.name}</li>
+        <li className="info-item" key={index}>{language.name}</li>
     ))
 
     const currencies = props.currencies.map((currency, index) => (
-        <li key={index}>{currency.name}</li>
+        <li className="info-item" key={index}>{currency.name}</li>
     ))
 
     const borders = props.borders.map((border, index) => (
-        <li key={index}><NavLink to={border} className="navLink"><button>{border}</button></NavLink></li>
+        <li className="borders-item" key={index}>
+            <NavLink to={border} className="navLink">
+                <button className="btn btn--borders">{border}</button>
+            </NavLink>
+        </li>
     ))
 
     return (
-        <div>
+        <div className="country-container">
             {/* <NavLink to={country.name} className="navLink"><p>{country.name}</p></NavLink> */}
-            <img src={props.flag} alt="" width="50" height="50" />
-            <NavLink to="/" className="navLink"><button>Back</button></NavLink>
-            <p>Name: {props.name}</p>
-            <p>NativeName: {props.nativeName}</p>
-            <p>Population: {props.population}</p>
-            <p>Region: {props.region}</p>
-            <p>SubRegion: {props.subRegion}</p>
-            <p>Capital: {props.capital}</p>
-            <p>Top Level Domain</p>
-            <ul>
-                {topLevelDomain}
-            </ul>
-            <p>Curriences</p>
-            <ul>
-                {currencies}
-            </ul>
-            <p>Languages</p>
-            <ul>
-                {languages}
-            </ul>
-            <ul>
-                {borders}
-            </ul>
+            <div className="country-container__back">
+                <NavLink to="/" className="navLink">
+                    <button className="btn country-container__button-back">Back</button>
+                </NavLink>
+
+            </div>
+            <div className="country-details">
+                <div className="country-details__flag">
+                    <img className="country-details__flagImg" src={props.flag} alt="" width="50" height="50" />
+                </div>
+                <div className="country-details__info">
+                    <h1 className="country-details__headingPrimary">{props.name}</h1>
+                    <ul className="info-list info-list--partOne">
+                        <li className="info-item">
+                            <h4 className="country-details__headingQuaternary">Native Name:</h4>{props.nativeName}
+                        </li>
+                        <li className="info-item">
+                            <h4 className="country-details__headingQuaternary">Population:</h4>{props.population}
+                        </li>
+                        <li className="info-item">
+                            <h4 className="country-details__headingQuaternary">Region:</h4>{props.region}
+                        </li>
+                        <li className="info-item">
+                            <h4 className="country-details__headingQuaternary">Sub Region:</h4>{props.subRegion}
+                        </li>
+                        <li className="info-item">
+                            <h4 className="country-details__headingQuaternary">Capital:</h4>{props.capital}
+                        </li>
+                    </ul>
+                    <ul className="info-list info-list--partTwo">
+                        <ul className="info-list__domain">
+                            <h4 className="country-details__headingQuaternary">Top Level Domain:</h4>{topLevelDomain}
+                        </ul>
+                        <ul className="info-list__curriences">
+                            <h4 className="country-details__headingQuaternary">Curriences: </h4>{currencies}
+                        </ul>
+                        <ul className="info-list__languages">
+                            <h4 className="country-details__headingQuaternary">Languages:</h4>{languages}
+                        </ul>
+                    </ul>
+                    <ul className="info-list info-list--partThree">
+
+                        <ul className="info-list__borders">
+                            <h4>Border Countries:</h4>
+                            {borders}
+                        </ul>
+                    </ul>
+                </div>
+            </div>
             {/* <p>{props.borders}</p> */}
         </div>
     );
