08709ddd0542f463a053100dc4ce663c7e26ab58
PioKl <kosiu342@gmail.com>
2020-03-20 19:32:37 +0100

    Add new class to img

--- src/components/Country.js
+++ src/components/Country.js
@@ -35,12 +35,12 @@ const Country = (props) => {
             </div>
             <div className="country-details">
                 <div className="country-details__flag">
-                    <img className="country-details__flagImg" src={props.flag} alt={`${props.name} flag`} />
+                    <img className={`country-details__flagImg country-details__flagImg--${props.alpha3Code}`} src={props.flag} alt={`${props.name} flag`} />
                 </div>
                 <div className="country-details__info">
                     <h1 className="country-details__headingPrimary">{props.name}</h1>
                     <ul className="info-list info-list--partOne">
-                        <li className="info-item">
+                        <li className="info-item info-item--nativeName">
                             <h4 className="country-details__headingQuaternary">Native Name:</h4>{props.nativeName}
                         </li>
                         <li className="info-item">
