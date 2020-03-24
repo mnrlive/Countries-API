683e969cd572f81ba2c1a1ef363cd98348084ddc
PioKl <kosiu342@gmail.com>
2020-03-24 19:37:45 +0100

    Add new functional component Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../style/Footer.scss";
+const Footer = () => {
+    return (
+        <footer className="footer">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer" className="footer__link">Frontend Mentor</a>.
+        Coded by <a href="https://github.com/PioKl" target="_blank" rel="noopener noreferrer" className="footer__link">Piotr Kłosowski</a>.
+        </footer>
+    );
+}
+
+export default Footer;
