aa885658c2d4e2a981001607b726495db914ba69
PioKl <kosiu342@gmail.com>
2020-03-24 19:39:00 +0100

    New loader and fix of scroll (CompnentWillUnmount)

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,18 +18,8 @@ class Home extends Component {
 
     componentDidMount() {
         //Nasłuchiwanie scrolla, który umożliwi powrót na początek strony
-        window.addEventListener('scroll', () => {
-            const beginScroll = window.scrollY < 100;
-            if (beginScroll === false) {
-                this.setState({
-                    scroll: true,
-                })
-            } else {
-                this.setState({
-                    scroll: false,
-                })
-            }
-        })
+        window.addEventListener('scroll', this.scrollCheck, false);
+
         fetch('https://restcountries.eu/rest/v2/all')
             .then(response => {
                 console.log(response.ok);
@@ -81,6 +71,24 @@ class Home extends Component {
                 .catch(error => console.log(error));
         }
 
+    }
+
+    componentWillUnmount() {
+        // you need to unbind the same listener that was binded.
+        window.removeEventListener('scroll', this.scrollCheck, false);
+    }
+
+    scrollCheck = () => {
+        const beginScroll = window.scrollY < 100;
+        if (beginScroll === false) {
+            this.setState({
+                scroll: true,
+            })
+        } else {
+            this.setState({
+                scroll: false,
+            })
+        }
     }
 
     handleSearchCountry = (e) => {
@@ -166,7 +174,7 @@ class Home extends Component {
                 </div>
                 {/* Warunek 1 jeśli dane jeszcze się nie załadowały, to niech wyświetli się napis Loading */}
                 {/* Warunek 2 jeśli tablica pofiltrowana jest pusta (bo nic nie ma w inpucie, bądź została wybrana jakaś opcja z droplisty) to wyświetl tablicę countries (czyli bezpośrednio z api), jeśli filtered nie jest puste to wyświetl kraje pofiltrowane */}
-                {this.state.isLoaded ? <Countries countriesAll={this.state.filtered === "" ? this.state.countriesAll : this.state.filtered} /> : <span className='loading'>Loading...</span>}
+                {this.state.isLoaded ? <Countries countriesAll={this.state.filtered === "" ? this.state.countriesAll : this.state.filtered} /> : <div className='loading'><span className='loader'></span></div>}
                 <div className={this.state.scroll ? 'scroll' : null} onClick={() => { window.scrollTo(0, 0) }}>
                     <i className={this.state.scroll ? "fas fa-arrow-up scroll__arrowUp" : null}></i>
                 </div>
