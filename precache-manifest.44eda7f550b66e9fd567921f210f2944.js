64bf61e7283e8b73b8c1917048128f77b879bf82
PioKl <kosiu342@gmail.com>
2020-03-26 18:45:54 +0100

    Updates

--- precache-manifest.44eda7f550b66e9fd567921f210f2944.js
+++ precache-manifest.44eda7f550b66e9fd567921f210f2944.js
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/REST-Countries-API/static/js/2.f611c60f.chunk.js"
   },
   {
-    "revision": "2f93b34052f35481f595c060d03572a1",
+    "revision": "370fa99ec6bcf715aab696f65bbde731",
     "url": "/REST-Countries-API/index.html"
   }
 ];
