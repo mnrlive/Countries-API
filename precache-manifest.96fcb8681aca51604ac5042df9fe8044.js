5e32f811d477e54a615b4ec1b02247286a9c144f
PioKl <kosiu342@gmail.com>
2020-03-30 13:23:07 +0200

    Updates

--- precache-manifest.96fcb8681aca51604ac5042df9fe8044.js
+++ precache-manifest.96fcb8681aca51604ac5042df9fe8044.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "a53c733d1400b4c977c6",
+    "revision": "1d98a109bf45178a05ed",
     "url": "/REST-Countries-API/static/css/main.4f0f8317.chunk.css"
   },
   {
-    "revision": "a53c733d1400b4c977c6",
-    "url": "/REST-Countries-API/static/js/main.52a8c69a.chunk.js"
+    "revision": "1d98a109bf45178a05ed",
+    "url": "/REST-Countries-API/static/js/main.62a444fa.chunk.js"
   },
   {
     "revision": "276c7ca0287b69b2f77d",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/REST-Countries-API/static/js/2.f611c60f.chunk.js"
   },
   {
-    "revision": "80269e4c5d95d175691a3d0eef037e6d",
+    "revision": "c78022ac0ba38c7f846103303ecee4e3",
     "url": "/REST-Countries-API/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/REST-Countries-API/precache-manifest.af7f8c9e1d35256d60428e1c8d971a40.js"
+  "/REST-Countries-API/precache-manifest.96fcb8681aca51604ac5042df9fe8044.js"
 );
 
 workbox.clientsClaim();
