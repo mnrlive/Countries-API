e849c943a54ed0bf7cc005f97e528f62a7e2da25
Pandimadhubabu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-09-11 13:48:28 +0000

    Deploying to gh-pages from  @ ec2c1050b3a5c16f64839e459cc95890ae84d5e6 🚀

--- precache-manifest.8ff23522e86ea6724fe84253d12c9fd2.js
+++ precache-manifest.8ff23522e86ea6724fe84253d12c9fd2.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "f014ee3b53f58f5b8cd7",
+    "revision": "cffc0adb3f3ce1190a00",
     "url": "/Countries-API/static/css/main.164f244a.chunk.css"
   },
   {
-    "revision": "f014ee3b53f58f5b8cd7",
-    "url": "/Countries-API/static/js/main.7217634c.chunk.js"
+    "revision": "cffc0adb3f3ce1190a00",
+    "url": "/Countries-API/static/js/main.85345cda.chunk.js"
   },
   {
     "revision": "4eb5ac3bf48440028a87",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/Countries-API/static/js/2.f611c60f.chunk.js"
   },
   {
-    "revision": "0cbac29d73fcb5f4f13ac3e72d75b5c4",
+    "revision": "d569002dca53383d41cba508f445124c",
     "url": "/Countries-API/index.html"
   }
 ];
