aebdfe72b75ab23faf5e3eebd179c9df1fe2dc43
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-09-11 14:09:38 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,7 @@ import "../style/Footer.scss";
 const Footer = () => {
     return (
         <footer className="footer">
-            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer" className="footer__link">Frontend Mentor</a>.
-        Coded by <a href="https://github.com/PioKl" target="_blank" rel="noopener noreferrer" className="footer__link">Piotr Kłosowski</a>.
+            
         </footer>
     );
 }
