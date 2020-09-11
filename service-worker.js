50c80592ca6175b646070fbfd2242d23a2cbf0f6
Pandimadhubabu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-09-11 08:41:02 +0000

    Deploying to gh-pages from  @ aebdfe72b75ab23faf5e3eebd179c9df1fe2dc43 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Countries-API/precache-manifest.1b6aa544826faa9f3f8d6ef40576d23b.js"
+  "/Countries-API/precache-manifest.14155c753d44dc3944e29f4a5eedf8d0.js"
 );
 
 workbox.clientsClaim();
