5d6299a9c82860e62957d447211208c816eb8c0d
Pandimadhubabu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-09-12 06:49:25 +0000

    Deploying to gh-pages from  @ aebdfe72b75ab23faf5e3eebd179c9df1fe2dc43 🚀

---
+++ precache-manifest.14155c753d44dc3944e29f4a5eedf8d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08d11a16f361d1c68abe",
+    "url": "/Countries-API/static/css/main.164f244a.chunk.css"
+  },
+  {
+    "revision": "08d11a16f361d1c68abe",
+    "url": "/Countries-API/static/js/main.4f3225fe.chunk.js"
+  },
+  {
+    "revision": "4eb5ac3bf48440028a87",
+    "url": "/Countries-API/static/js/runtime~main.52a4f1bd.js"
+  },
+  {
+    "revision": "5dadcb849ce28abb8586",
+    "url": "/Countries-API/static/js/2.f611c60f.chunk.js"
+  },
+  {
+    "revision": "9155d70acabaf4e79f25ac360e00233d",
+    "url": "/Countries-API/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Countries-API/precache-manifest.fb7e0b434f705e348e81516926793e38.js"
+  "/Countries-API/precache-manifest.14155c753d44dc3944e29f4a5eedf8d0.js"
 );
 
 workbox.clientsClaim();
